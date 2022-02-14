bd2bb1ea860f52b4e478ca31f4967a09c49d91ea
TheHerbin <59963115+TheHerbin@users.noreply.github.com>
2022-02-14 16:57:48 +0100

    Ajout db

---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UtilisateurRepository::class)
+ */
+class Utilisateur
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $pseudo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $passwd;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $navigateur;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=true)
+     */
+    private $ip;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $token;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(?string $pseudo): self
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPasswd(): ?string
+    {
+        return $this->passwd;
+    }
+
+    public function setPasswd(string $passwd): self
+    {
+        $this->passwd = $passwd;
+
+        return $this;
+    }
+
+    public function getNavigateur(): ?string
+    {
+        return $this->navigateur;
+    }
+
+    public function setNavigateur(?string $navigateur): self
+    {
+        $this->navigateur = $navigateur;
+
+        return $this;
+    }
+
+    public function getIp(): ?string
+    {
+        return $this->ip;
+    }
+
+    public function setIp(?string $ip): self
+    {
+        $this->ip = $ip;
+
+        return $this;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token): self
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+}
