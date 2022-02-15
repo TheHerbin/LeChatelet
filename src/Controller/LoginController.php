d2c253d1221e2c20cb56ef0b168eecd8bfcf4f21
Jeanjean <adrien.jeanjean@epsi.fr>
2022-02-15 17:28:19 +0100

    UPDATE

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -27,7 +27,6 @@ class LoginController extends AbstractController
      */
     public function index(UtilisateurRepository $utilisateurRepository): Response
     {
-
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
         ]);
