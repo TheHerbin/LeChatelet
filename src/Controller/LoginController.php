7d4b87b3b72308f9f74d3c90ec48b38821af0ad5
TheHerbin <59963115+TheHerbin@users.noreply.github.com>
2022-03-10 15:01:43 +0100

    fix Utilisateur

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -36,6 +36,8 @@ class LoginController extends AbstractController
      */
     public function index(UtilisateurRepository $utilisateurRepository, $twig): Response
     {
+
+
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
         ]);
