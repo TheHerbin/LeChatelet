0e6e7db92e89235085c885753052962afef56740
TheHerbin <theo.herbin59710@outlook.fr>
2022-03-20 15:41:11 +0100

    MERGE THEO ADRIEN

--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
+session_start();
+$BASE_URL = "/LeChatelet/public";
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -15,7 +15,13 @@ use App\Repository\UtilisateurRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\PngWriter;
+use Scheb\TwoFactorBundle\Model\Google\TwoFactorInterface as GoogleAuthenticatorTwoFactorInterface;
+use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Google\GoogleAuthenticatorInterface;
 
 
 class LoginController extends AbstractController
@@ -25,93 +31,120 @@ class LoginController extends AbstractController
      */
     public function index(UtilisateurRepository $utilisateurRepository, Request $request, ManagerRegistry $doctrine): Response
     {
+        $ldap_dn = "dc=clinique,dc=chatelet,dc=com";
+        $ldap_password = "";
+        $ldap_tree = "OU=SBSUsers,OU=Users,OU=MyBusiness,DC=myDomain,DC=local";
+        $ldap_con = ldap_connect("192.168.1.59");
 
-        // dump($request->request->get('email'));
+        ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3) or die('Unable to set LDAP protocol version');
+        ldap_set_option($ldap_con, LDAP_OPT_REFERRALS, 0); 
+        try{
+            ldap_bind($ldap_con, $ldap_dn, $ldap_password);
+        }catch(\Exception $e){
+            return $this->redirectToRoute('maintenance');
+        }
 
         $form = array();
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $ldap_dn = "dc=clinique,dc=chatelet,dc=com";
-            $ldap_password = "";
-            $ldap_con = ldap_connect("192.168.1.59");
-
-            dump($ldap_con);
-
-
-            if (ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
+           
+            //Code pour connecter l'ad
+        
+            if (TRUE === ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
                 echo 'Bind Success';
-
+                /*   $search_filter = '(cn=)';
+                        $attributes = array();
+                        $attributes[] = 'givenname';
+                        $attributes[] = 'mail';
+                        $attributes[] = 'sn';
+                        $result = ldap_search($ldap_con, $ldap_dn, $search_filter, $attributes);
+                        $entries = ldap_get_entries($ldap_connection, $result);
+        
+                        print_r($result);
+                        print_r($entries);
+                        */
 
                 $form['valide'] = true;
                 $email = $_POST['email'];
                 $mdp = $_POST['mdp'];
 
                 $ip = null;
-                //Code pour connecter l'ad
+
                 //if présence dans l'annuaire AD ici
+                //A FAIRE
+                    //if yes 
+                    //récup de l'user ciblé
+                    $utilisateur = $utilisateurRepository->findOneByEmail($request->request->get('email'))[0];
+                    //Comparaison des MDP hashés via l'ad et email
+                
+                    //Code pour récup l'ip et navigateur en fonction du proxy/ VPN : 
+                    $ip = getIPAddress();
+                    $navigateur = get_browser_name($_SERVER['HTTP_USER_AGENT']);
 
-                //if yes 
-                $utilisateur = $utilisateurRepository->findOneByEmail($email);
-                print_r($utilisateur);
-                //Comparaison des MDP hashés via l'ad
+                    // if $utilisateur->navigateur || $utilisateur->ip !== $ip
+                    if($ip !== $utilisateur->getIp() || $navigateur !== $utilisateur->getNavigateur()){
+                        // if yes
+                        // Lancer vérification authentificator QR code si non enregistré ou demander code si enregistré en DB
 
-                //Code pour récup l'ip en fonction du proxy : 
-                $ip = getIPAddress();
-
-                // if $utilisateur->navigateur || $utilisateur->ip !== $ip
-                // if yes
-                // Lancer vérification authentificator QR code si non enregistré ou demander code si enregistré en DB
-
-                //VERIFICATION QR CODE
-
-                if ($request->request->get('email')) {
-                    $utilisateur = $utilisateurRepository->findOneByEmail($request->request->get('email'));
-                    if ($utilisateur == null) {
-                        //Il n'y a pas d'utilisateur donc en en ajoute un dans la bdd
-                        $entityManager = $doctrine->getManager();
-
-                        $user = new Utilisateur();
-                        $user->setEmail($request->request->get('email'));
-
-                        //On initialise dans la bdd le navigateur par default
-                        $user->setNavigateur(get_browser_name($_SERVER['HTTP_USER_AGENT']));
-
-                        $user->setIp(getIPAddress());
-
-                        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-                        $entityManager->persist($user);
-
-                        // actually executes the queries (i.e. the INSERT query)
-                        $entityManager->flush();
-
-                        $user = $utilisateurRepository->findOneByEmail(($request->request->get('email')));
-
-                        //redirection avec parametre mail
-                        return $this->redirectToRoute('app_google_register_code', ['id' => $user[0]->getId()]);
-                    } else {
-                        //Verification google code
-
-                        if ($utilisateur[0]->getGoogleAuthenticatorSecret() == null) {
-
-                            return $this->redirectToRoute('app_google_register_code', ['id' => $utilisateur[0]->getId()]);
-                        } else {
-
-                            return $this->redirectToRoute('app_verif', ['id' => $utilisateur[0]->getId()]);
-                        }
+                            $utilisateur = $utilisateurRepository->findOneByEmail($request->request->get('email'));
+                            if ($utilisateur == null) {
+                                //Il n'y a pas d'utilisateur donc en en ajoute un dans la bdd
+                                $entityManager = $doctrine->getManager();
+    
+                                $user = new Utilisateur();
+                                $user->setEmail($request->request->get('email'));
+    
+                                //On initialise dans la bdd le navigateur par default
+                                $user->setNavigateur(get_browser_name($_SERVER['HTTP_USER_AGENT']));
+    
+                                $user->setIp(getIPAddress());
+    
+                                // tell Doctrine you want to (eventually) save the Product (no queries yet)
+                                $entityManager->persist($user);
+    
+                                // actually executes the queries (i.e. the INSERT query)
+                                $entityManager->flush();
+    
+                                $user = $utilisateurRepository->findOneByEmail(($request->request->get('email')));
+    
+                                //redirection avec parametre mail
+                                return $this->redirectToRoute('app_google_register_code', ['id' => $user[0]->getId()]);
+                            } else {
+                                //Verification google code
+    
+                                if ($utilisateur[0]->getGoogleAuthenticatorSecret() == null) {
+    
+                                    return $this->redirectToRoute('app_google_register_code', ['id' => $utilisateur[0]->getId()]);
+                                } else {
+    
+                                    return $this->redirectToRoute('app_verif', ['id' => $utilisateur[0]->getId()]);
+                                }
+                            }
+                        
+                    }else{
+                        $_SESSION['USER'] = true ;
+                        $isLoggedIn = $_SESSION['USER'];
+                        return $this->render('home/index.html.twig', array(
+                            'controller_name' => 'HomeController',
+                            'isLogged' => $isLoggedIn
+                        ));
                     }
-                }
-
-
-                // if no
-                // Rediriger vers l'accueil avec les variables session pour permettre l'affichage des infos utilisateurs.
+                    // if no
+                    // Rediriger vers l'accueil avec les variables session pour permettre l'affichage des infos utilisateurs.
 
                 // if no 
-
+                //return $this->redirect('https://google.fr');
                 // blacklist IP
+            }else{
+                return $this->redirectToRoute('maintenance');
             }
+
+            
         }
+        $isLoggedIn = isset($_SESSION['USER']);
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
+            'isLogged' => $isLoggedIn
         ]);
     }
 }
